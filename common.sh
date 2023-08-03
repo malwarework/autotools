d4be7e9ecdcf4060807a319362a1db94725a567f
malwarework <7050721+malwarework@users.noreply.github.com>
2023-08-04 00:05:38 +0300

    Update common.sh

--- common.sh
+++ common.sh
@@ -41,7 +41,7 @@ python3 -c 'import pty; pty.spawn("/bin/bash")'
 python -c 'import pty; pty.spawn("/bin/bash")'
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/tmp
 export TERM=xterm-256color
-alias ll='clear; ls -lsaht --color=aut0'
+alias ll='clear; ls -lsaht --color=auto'
 Ctrl + Z [Background Process]
 stty raw -echo ; fg ; reset
 stty columns 200 rows 200
