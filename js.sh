fa4f8ca7920a02c90d3457d4ca6c6bcea0b0e418
malwarework <7050721+malwarework@users.noreply.github.com>
2024-01-08 14:14:16 +0300

    Create js.sh

---
+++ js.sh
@@ -0,0 +1,2 @@
+subfinder -d domain.com | httpx -mc 200 | tee subdomains.txt && cat subdomains.txt | waybackurls | httpx -mc 200 | grep .js | tee js.txt
+nuclei -l js.txt -t ~/nuclei-templates/exposures/ -o js_exposures_results.txt
