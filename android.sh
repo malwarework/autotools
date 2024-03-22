0a0394c6928d31c29120e01ec4d7ec370452811a
malwarework <7050721+malwarework@users.noreply.github.com>
2024-03-22 14:28:49 +0300

    Update android.sh

--- android.sh
+++ android.sh
@@ -1,6 +1,8 @@
 ============DROZER=============
 adb forward tcp:31415 tcp:31415
+adb forward --remove-all
 =============BURP==============
 adb shell settings put global http_proxy 127.0.0.1:8080
 adb reverse tcp:8080 tcp:8080
+adb reverse --remove-all
 ===============================
